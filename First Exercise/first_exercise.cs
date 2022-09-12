a9cb35e7661af02bca415a4896c8bb7b5258fc5a
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-12 21:05:42 +0300

    First Excercise finished from SoftUni C# PB!

---
+++ First Exercise/first_exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_Exercise
+{
+    internal class first_exercise
+    {
+        static void Main()
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.7954;
+
+            Console.WriteLine(bgn);
+        }
+    }
+}
