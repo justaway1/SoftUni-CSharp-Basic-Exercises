52e8b2124a1583aa586b2b1e7754e5354bc33b9c
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-12 21:10:43 +0300

    Second Excercise from SoftUni finished!

---
+++ Second_Exercise/second_exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Second_Exercise
+{
+    internal class second_exercise
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double radius = radians * 180 / Math.PI;
+
+            Console.WriteLine(radius);
+        }
+    }
+}
