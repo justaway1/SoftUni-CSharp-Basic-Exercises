3898e6e83423084b35ee087fece6beada141b7f4
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-12 21:18:43 +0300

    Third Excercise finished from SoftUni C# PB

---
+++ Third_Exercise/Third_Exercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Third_Exercise
+
+
+{
+    internal class Third_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double yearTax = double.Parse(Console.ReadLine());
+
+            double calculateSum = deposit + months * ((deposit * yearTax / 100) / 12);
+
+            Console.WriteLine(calculateSum);
+
+        }
+    }
+}
