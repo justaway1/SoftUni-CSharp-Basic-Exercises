2fafea15e3f4dfa4368495613a5706642d5e4dbd
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-13 19:39:55 +0300

    Fourth Exercise completed!

---
+++ Fourth_Exercise/Fourth_Exercise.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Fourth_Exercise
+
+{
+    internal class Fourth_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int bookPages = int.Parse(Console.ReadLine());
+            int readPages = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            //Да се отпечата на конзолата броят часове, които Жоро трябва да отделя за четене всеки ден.
+            // 200 страници    // 30 за час = 
+            int hoursNeeded = (bookPages / readPages) / days;
+
+            Console.WriteLine(hoursNeeded);
+        }
+    }
+}
