0383b2ba0498a6791e57982a14c8850a1506bc9e
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-13 19:41:40 +0300

    Fifth Exercise Completed

---
+++ Fifth_Exercise/Fifth_Exercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fifth_Exercise
+{
+    internal class Fifth_Exercise
+    {
+        static void Main(string[] args)
+        {
+
+            double pens = double.Parse(Console.ReadLine()) * 5.80;
+            double markers = double.Parse(Console.ReadLine()) * 7.20;
+            double spray = double.Parse(Console.ReadLine()) * 1.20;
+            int discount = int.Parse(Console.ReadLine());
+
+            double finalPrice = pens + markers + spray;
+            double finalDiscount = finalPrice - ((finalPrice * discount) / 100);
+
+            Console.WriteLine(finalDiscount);
+
+        }
+    }
+}
