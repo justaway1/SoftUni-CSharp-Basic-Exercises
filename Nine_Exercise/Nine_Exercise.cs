c3c0fed9e1a423ef68bb45570cd19644a9472e76
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-14 20:24:59 +0300

    Ninth Challange Completed from SoftUni C# basics

---
+++ Nine_Exercise/Nine_Exercise.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Nine_Exercise
+{
+    internal class Nine_Exercise
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            //1. Да се напише програма, която изчислява литрите вода, която са необходими за напълването на аквариума.
+
+            //1.Дължина в см – цяло число в интервала[10 … 500]
+
+            //2.Широчина в см – цяло число в интервала[10 … 300]
+
+            //3.Височина в см – цяло число в интервала[10… 200]
+
+            //4.Процент – реално число в интервала[0.000 … 100.000]
+
+
+            int longNeeded = int.Parse(Console.ReadLine());
+            int widthNeeded = int.Parse(Console.ReadLine());
+            int heightNeeded = int.Parse(Console.ReadLine());
+            double percentNeeded = double.Parse(Console.ReadLine());
+
+            //Да се отпечата на конзолата едно число:
+
+            double volume = longNeeded * widthNeeded * heightNeeded;
+            double volumeLitre = volume / 1000;
+            double spaceNeeded = percentNeeded / 100;
+            double litreNeeded = volumeLitre * (1 - spaceNeeded);
+
+            //· литрите вода, които ще събира аквариума.
+
+
+            Console.WriteLine(litreNeeded);
+
+
+        }
+    }
+}
