46a32d70263217e6ae2659eecae3140608bb7a9c
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-18 16:07:46 +0300

    The 10 Exercise finished!

---
+++ Ten_Exercise/Ten_Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Ten_Exercise
+{
+    internal class Ten_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if(grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            } 
+        }
+    }
+}
