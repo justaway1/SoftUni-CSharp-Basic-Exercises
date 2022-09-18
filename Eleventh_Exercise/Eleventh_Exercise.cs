1fa19c77bdf883dee7e5f0743b4d3afeb11bac17
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-18 16:09:25 +0300

    The 11 Exercise Completed!

---
+++ Eleventh_Exercise/Eleventh_Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Eleventh_Exercise
+{
+    internal class Eleventh_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber >= secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+
+            }
+            else if (secondNumber >= firstNumber)
+            {
+                Console.WriteLine(secondNumber);
+            }
+        }
+    }
+}
