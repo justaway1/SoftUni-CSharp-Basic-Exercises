3e1df794feb25522f2e08f5d77d25eafe14dee8e
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-18 16:09:51 +0300

    The 12 Exercise finished!

---
+++ 12_Exercise/Twelved_Exercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12_Exercise
+{
+    internal class Twelved_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+
+            if(num1 % 2 == 0)
+            {
+                Console.WriteLine("even");
+            } else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
