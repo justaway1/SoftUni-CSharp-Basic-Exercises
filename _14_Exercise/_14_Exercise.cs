f4eb5c0dcea550fd9af7e96c0b68b3b3486ec99a
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-19 18:30:52 +0300

    14 Exercise Completed!

---
+++ _14_Exercise/_14_Exercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _14_Exercise
+{
+    internal class _14_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int guessNumber = int.Parse(Console.ReadLine());
+
+
+            if (guessNumber < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+             else if (guessNumber >= 100 && guessNumber <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
