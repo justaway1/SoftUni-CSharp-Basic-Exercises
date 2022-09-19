70c1e41ef6e125c785f714327a8d0e800a137f34
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-19 18:30:35 +0300

    13 Exercise Completed

---
+++ _13_Exercise/_13_Exercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _13_Exercise
+{
+    internal class _13_Exercise
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            } else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
