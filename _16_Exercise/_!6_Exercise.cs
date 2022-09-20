dc1a79f38cb24368e820f07660aacf1c50ff2e77
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-20 19:04:12 +0300

    16 Exercise Completed from SoftUni

---
+++ _16_Exercise/_!6_Exercise.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace _16_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                var side = double.Parse(Console.ReadLine());
+                Console.WriteLine(side * side);
+            }
+            else if (figure == "rectangle")
+            {
+                var side = double.Parse(Console.ReadLine());
+                var side2 = double.Parse(Console.ReadLine());
+                Console.WriteLine(side * side2);
+            }
+            else if (figure == "circle")
+            {
+                var side = double.Parse(Console.ReadLine());
+                Console.WriteLine((side * side) * Math.PI);
+            }
+            else if (figure == "triangle")
+            {
+                var side = double.Parse(Console.ReadLine());
+                var side1 = double.Parse(Console.ReadLine());
+                Console.WriteLine((side * side1) / 2);
+            }
+        }
+    }
+}
