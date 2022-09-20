7118c58f5c41bf1faad60900307c01572f719420
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-20 19:04:53 +0300

    18 Exercise Completed From SoftUni CSharp PB

---
+++ _18_Exercise/_18_Exercise.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _18_Exercise
+{
+    internal class _18_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double bonus = 0;
+
+            if (number <= 100)
+            {
+                bonus = 5;
+            }
+            else if (number > 1000)
+            {
+                bonus = number * 0.1;
+            }
+            else
+            {
+                bonus = number * 0.2;
+            }
+
+            if (number % 2 == 0)
+            {
+                bonus += 1;
+            } 
+            else if (number % 10 == 5)
+            {
+                bonus += 2;
+            }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus + number);
+
+
+
+
+        }
+    }
+}
