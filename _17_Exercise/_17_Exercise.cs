1959f622fbe3d52118a9b5809340a7aad7448c8c
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-20 19:04:29 +0300

    17 Exercise Completed!

---
+++ _17_Exercise/_17_Exercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _17_Exercise
+{
+    internal class _17_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int total = firstNumber + secondNumber + thirdNumber;
+
+            int minutes = total / 60;
+            int seconds = total % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+
+        }
+    }
+}
