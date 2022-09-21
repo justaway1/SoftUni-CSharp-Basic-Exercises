1f077d7273f4402277415c2f2fb41d94e1396da6
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-21 20:11:10 +0300

    20 Exercise Completed!

---
+++ _20_Exercise/_20_Exercise.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _20_Exercise
+{
+    internal class _20_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            double bonusMinutes = minutes + 15;
+
+
+            if (hours >= 23 && minutes >= 55)
+            {
+                hours = 0;
+                bonusMinutes %= 20;
+                Console.WriteLine($"{hours}:{bonusMinutes}");
+            }
+            else if (hours >= 23 && minutes >= 45 && minutes <= 54)
+            {
+                hours = 0;
+                bonusMinutes %= 10;
+                Console.WriteLine($"{hours}:0{bonusMinutes}");
+            }
+
+            else if (minutes >= 45 && minutes <= 54)
+            {
+                bonusMinutes %= 10;
+                hours++;
+                Console.WriteLine($"{hours}:0{bonusMinutes}");
+            }
+
+            else if (minutes >= 55)
+            {
+                hours++;
+                bonusMinutes %= 20;
+                Console.WriteLine($"{hours}:{bonusMinutes}");
+            }
+
+            else
+            {
+                Console.WriteLine($"{hours}:{bonusMinutes}");
+            }
+
+        }
+    }
+}
