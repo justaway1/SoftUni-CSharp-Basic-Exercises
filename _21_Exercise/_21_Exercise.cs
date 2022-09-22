f60070c02cbd25822c7e028fc5a84af86718ba6e
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-22 16:51:51 +0300

    21 Exercise Finished! C# PB!

---
+++ _21_Exercise/_21_Exercise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _21_Exercise
+{
+    internal class _21_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double suits = double.Parse(Console.ReadLine());
+
+            double priceDecor = budget * 0.10;
+            double priceSuits = statists * suits;
+            double totalSpent = priceDecor + priceSuits;
+
+
+
+            if (statists >= 150)
+            {
+                double discountedPrice = priceSuits - (priceSuits * 0.10);
+                totalSpent = discountedPrice + priceDecor;
+            }
+
+            if (totalSpent > budget)
+            {
+                double leftedMoney = totalSpent - budget;
+                Console.WriteLine($"Not enough money! \nWingard needs {leftedMoney.ToString("0.00")} leva more.");
+            }
+            else
+            {
+                double leftedMoney = budget - totalSpent;
+                Console.WriteLine($"Action! \nWingard starts filming with {leftedMoney.ToString("0.00")} leva left.");
+            }
+        }
+    }
+}
