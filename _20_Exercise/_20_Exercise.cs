8e5469c9da08e2cbd4cbecb76b0752334d114dc7
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-22 16:51:29 +0300

    20 Exercise Completed From SoftUni C#

--- _20_Exercise/_20_Exercise.cs
+++ _20_Exercise/_20_Exercise.cs
@@ -9,40 +9,46 @@ namespace _20_Exercise
             int hours = int.Parse(Console.ReadLine());
             int minutes = int.Parse(Console.ReadLine());
 
-            double bonusMinutes = minutes + 15;
+            int bonusMinutes = minutes + 15;
+
+            hours += bonusMinutes / 60;
+            bonusMinutes %= 60;
+            hours %= 24;
+
+            Console.WriteLine($"{hours}:{bonusMinutes:D2}");
 
 
-            if (hours >= 23 && minutes >= 55)
-            {
-                hours = 0;
-                bonusMinutes %= 20;
-                Console.WriteLine($"{hours}:{bonusMinutes}");
-            }
-            else if (hours >= 23 && minutes >= 45 && minutes <= 54)
-            {
-                hours = 0;
-                bonusMinutes %= 10;
-                Console.WriteLine($"{hours}:0{bonusMinutes}");
-            }
+            //if (hours >= 23 && minutes >= 55)
+            //{
+            //    hours = 0;
+            //    bonusMinutes %= 20;
+            //    Console.WriteLine($"{hours}:{bonusMinutes}");
+            //}
+            //else if (hours >= 23 && minutes >= 45 && minutes <= 54)
+            //{
+            //    hours = 0;
+            //    bonusMinutes %= 10;
+            //    Console.WriteLine($"{hours}:0{bonusMinutes}");
+            //}
 
-            else if (minutes >= 45 && minutes <= 54)
-            {
-                bonusMinutes %= 10;
-                hours++;
-                Console.WriteLine($"{hours}:0{bonusMinutes}");
-            }
+            //else if (minutes >= 45 && minutes <= 54)
+            //{
+            //    bonusMinutes %= 10;
+            //    hours++;
+            //    Console.WriteLine($"{hours}:0{bonusMinutes}");
+            //}
 
-            else if (minutes >= 55)
-            {
-                hours++;
-                bonusMinutes %= 20;
-                Console.WriteLine($"{hours}:{bonusMinutes}");
-            }
+            //else if (minutes >= 55)
+            //{
+            //    hours++;
+            //    bonusMinutes %= 20;
+            //    Console.WriteLine($"{hours}:{bonusMinutes}");
+            //}
 
-            else
-            {
-                Console.WriteLine($"{hours}:{bonusMinutes}");
-            }
+            //else
+            //{
+            //    Console.WriteLine($"{hours}:{bonusMinutes}");
+            //}
 
         }
     }
