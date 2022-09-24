957b8f076e3206f5b4f57e3d284a5ed47c7f0b33
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-24 23:21:56 +0300

    26 Exercise is Complete!

---
+++ _26_Exercise/_26_Exercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _26_Exercise
+{
+    internal class _26_Exercise
+    {
+        static void Main(string[] args)
+        {
+
+            string workingDays = Console.ReadLine();
+
+            if (workingDays == "Monday" || workingDays == "Tuesday"
+                 || workingDays == "Wednesday" || workingDays == "Thursday"
+                 || workingDays == "Friday")
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (workingDays == "Saturday" || workingDays == "Sunday")
+            {
+                Console.WriteLine("Weekend");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
