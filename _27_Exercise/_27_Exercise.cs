da9f15158f0bb4006c2644e517126682dbeaf069
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-25 21:40:33 +0300

    27 Exercise from SoftUni Completed

---
+++ _27_Exercise/_27_Exercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _27_Exercise
+{
+    internal class _27_Exercise
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+
+            if (animal == "dog")
+            {
+                Console.WriteLine("mammal");
+            }
+            else if (animal == "crocodile" ||
+                     animal == "tortoise" ||
+                     animal == "snake")
+            {
+                Console.WriteLine("reptile");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+
+        }
+    }
+}
