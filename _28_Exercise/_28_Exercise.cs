4713ee0d2e59fd0b2d2fd2947a2bdbea0cc971b1
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-25 21:40:59 +0300

    28 Exercise from SoftUni Completed

---
+++ _28_Exercise/_28_Exercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _28_Exercise
+{
+    internal class _28_Exercise
+    {
+        static void Main(string[] args)
+        {
+
+            double age = double.Parse(Console.ReadLine());
+            string title = Console.ReadLine();
+
+            if (title == "m")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else if (title == "f")
+
+                if (age >= 16)
+
+                {
+                    Console.WriteLine("Ms.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+
+        }
+    }
+}
