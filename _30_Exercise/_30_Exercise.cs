77e892cdf5b005bab8502d929613a3853873ffbd
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-25 21:41:39 +0300

    30 Exercise Completed

---
+++ _30_Exercise/_30_Exercise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _30_Exercise
+{
+    internal class _30_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                if (number > -0.01 && number < 1 )
+                {
+                    Console.WriteLine("No");
+
+                }
+            }
+
+            else if (number >= -100 && number <= 100)
+            {
+                Console.WriteLine("Yes");
+            }
+
+        }
+    }
+}
