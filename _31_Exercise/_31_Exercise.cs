9a3a240302c7eb00bfd73c80708253ffcc9f84d2
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-27 12:05:10 +0300

    31 Exercise Completed

---
+++ _31_Exercise/_31_Exercise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _31_Exercise
+{
+    internal class _31_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int workHours = int.Parse(Console.ReadLine());
+            string workDay = Console.ReadLine();
+
+            if (workDay == "Monday" || workDay == "Tuesday" || workDay == "Wednesday"
+                || workDay == "Thursday" || workDay == "Friday" || workDay == "Saturday")
+            {
+                if (workHours >= 10 && workHours <= 18)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+
+            }
+            else if (workDay == "Sunday")
+            { 
+                {
+                    Console.WriteLine("closed");
+                }
+            }
+        }
+    }
+}
