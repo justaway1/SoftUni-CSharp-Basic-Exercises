3fc4af61f6a59ed948deeec7512a919f349fa117
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-27 12:06:17 +0300

    33 Exercise Completed!

---
+++ _34_Exercise/_34_Exercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _34_Exercise
+{
+    internal class _34_Exercise
+    {
+        static void Main(string[] args)
+        {
+            int rangeNumber = int.Parse(Console.ReadLine());
+
+            if (rangeNumber == 0 && rangeNumber >= -0)
+            {
+                if (rangeNumber >= 100 && rangeNumber <= 200)
+                {
+                    Console.WriteLine();
+
+                }
+            }
+            else if (rangeNumber < 100 || rangeNumber > 200)
+            {
+                Console.WriteLine("invalid");
+            }
+
+        }
+    }
+}
