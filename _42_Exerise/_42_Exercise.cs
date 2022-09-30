06d899d3930a01b141f0756a11a900f6235b0e29
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-09-30 11:44:56 +0300

    42 Exercise Completed!

---
+++ _42_Exerise/_42_Exercise.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _42_Exerise
+{
+    internal class _42_Exercise
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            string myInput = Console.ReadLine();
+
+
+            if (myInput == "+")
+            {
+                double finalResult = firstNumber + secondNumber;
+                if (finalResult % 2 == 0)
+                {
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {finalResult} - even");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {finalResult} - odd");
+                }
+            }
+
+            else if (myInput == "-")
+            {
+                double finalResult = firstNumber - secondNumber;
+                if (finalResult % 2 == 0)
+                {
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {finalResult} - even");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = {finalResult} - odd");
+                }
+            }
+
+            else if (myInput == "/")
+            {
+                double finalResult = firstNumber / secondNumber;
+
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine($"Cannot divide {firstNumber} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = {finalResult:0.00}");
+
+                }
+
+            }
+
+            else if (myInput == "%")
+            {
+                double finalResult = firstNumber % secondNumber;
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine($"Cannot divide {firstNumber} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNumber} % {secondNumber} = {finalResult}");
+                }
+
+
+            }
+
+            else if (myInput == "*")
+            {
+                double finalResult = firstNumber * secondNumber;
+                if (finalResult % 2 == 0)
+                {
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {finalResult} - even");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {finalResult} - odd");
+                }
+            }
+        }
+    }
+}
