d565208baf1c5a93d7ef9cef545dc016971c5946
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-02 11:31:17 +0300

    Challanges Completed

---
+++ _44_Exercise/_44_Exercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _44_Exercise
+{
+    internal class _44_Exercise
+    {
+        static void Main(string[] args)
+        {
+            //  Първият ред съдържа час на изпита – цяло число от 0 до 23
+            //· Вторият ред съдържа минута на изпита – цяло число от 0 до 59
+            //· Третият ред съдържа час на пристигане – цяло число от 0 до 23
+            //· Четвъртият ред съдържа минута на пристигане – цяло число от 0 до 59
+
+            //Inputs
+            int examHour = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+            int studentHour = int.Parse(Console.ReadLine());
+            int studentMinutes = int.Parse(Console.ReadLine());
+
+            int remainingHour = 0;
+            int remainingMinutes = (examMinutes + studentMinutes) % 60;
+
+
+
+            if (examHour == studentHour && examMinutes == studentMinutes)
+            {
+                Console.WriteLine("On time");
+            }
+            
+
+
+
+
+
+
+
+        }
+    }
+}
