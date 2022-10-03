3aabb1a89e9451e61be93031e366b72f8b2f61d6
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-03 22:27:46 +0300

    another Challange/Exercise Completed

---
+++ NumbersN/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumbersN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber = int.Parse(Console.ReadLine());
+
+            for (int i = randomNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+
+    
+}
