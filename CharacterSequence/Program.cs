ff9c73642598936670dbb1006964037bcf303f0f
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-03 22:26:18 +0300

    Another Challange Completed

---
+++ CharacterSequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomWord = Console.ReadLine();
+
+            for (int i = 0; i < randomWord.Length; i++)
+            {
+                Console.WriteLine(randomWord[i]);
+            }
+        }
+    }
+}
