777bbdb85acffaf685de89da5e6f90bcf479f70d
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-03 22:26:49 +0300

    Challange / Exercise Completed!

---
+++ EvenPowers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EvenPowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= randomNumber; i+= 2)
+            {
+                
+                    Console.WriteLine(Math.Pow(2, i));
+
+            }
+        }
+    }
+}
