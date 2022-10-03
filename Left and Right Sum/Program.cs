be8b12bbd1f7c0ae76870b7191f17b77b066d2a8
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-03 22:27:19 +0300

    Exercise / Challange Completed

---
+++ Left and Right Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int firstNumbersSum = 0;
+            int secondNumbersSum = 0;
+            int finalResult = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                int firstNumbers = int.Parse(Console.ReadLine());
+
+                firstNumbersSum += firstNumbers;
+
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                int secondNumbers = int.Parse(Console.ReadLine());
+
+                secondNumbersSum += secondNumbers;
+            }
+
+            if (firstNumbersSum == secondNumbersSum)
+            {
+                finalResult += firstNumbersSum;
+                Console.WriteLine($"Yes, sum = {finalResult}");
+            }
+            else if (firstNumbersSum != secondNumbersSum)
+            {
+                finalResult = Math.Abs(firstNumbersSum - secondNumbersSum);
+                Console.WriteLine($"No, diff = {finalResult}");
+            }
+
+
+
+        }
+    }
+}
