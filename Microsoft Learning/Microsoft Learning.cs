1a92d0f37a620fc95baafd0a769d6155cf89a15f
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-10-08 13:47:14 +0300

    Exercise Completed

---
+++ Microsoft Learning/Microsoft Learning.cs
@@ -0,0 +1,26 @@
+﻿using Humanizer;
+using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics;
+using System.Collections;
+
+
+
+//Return Directory Folders in folder Lola
+IEnumerable<string> listofDirectories = Directory.EnumerateDirectories("Lola", "*", SearchOption.AllDirectories);
+
+foreach (var dir in listofDirectories)
+{
+    Console.WriteLine(dir);
+}
+
+
+
+//This return files within that folder Lola
+//IEnumerable<string> files = Directory.EnumerateFiles("Lola", "*.txt", SearchOption.AllDirectories);
+
+
+//foreach (var file in files)
+//{
+//    Console.WriteLine(file);
+//}
